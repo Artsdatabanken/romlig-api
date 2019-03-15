d6bec76195e0d36d2f8fdafab6b8a8d793704e78
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-15 13:58:55 +0100

    Gaussian: Fix dipp på enden av skala

--- src/gaussian.js
+++ src/gaussian.js
@@ -1,12 +1,12 @@
-const gauss = [0.5 * 0.1914624613, 0.1498822848, 0.09184805266, 0.04405706932]; // 0.4 ...
+const gauss = [0.5 * 0.1914624613, 0.1498822848, 0.09184805266, 0.04405706932]; // 0.5 ...
 
 function gaussian(data) {
   const len = data.length;
   const r = new Array(len).fill(0);
   for (let i = 0; i < data.length; i++) {
     for (let offset = 0; offset < gauss.length; offset++) {
-      if (i + offset < len) r[i] += gauss[offset] * data[i + offset];
-      if (i - offset >= 0) r[i] += gauss[offset] * data[i - offset];
+      r[i] += gauss[offset] * data[Math.min(len - 1, i + offset)];
+      r[i] += gauss[offset] * data[Math.max(0, i - offset)];
     }
   }
   return r;
