7ae126dc090a4e76c3a19d2f1770821b3961d2f0
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-06 20:16:40 +0200

    Decode URI query string args

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ const api = require("./api");
 module.exports = function(app, config) {
   app.get("/statistikk/grid1d", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    const punkter = req.query.punkter;
-    const raster = req.query.raster;
+    const punkter = decodeURI(req.query.punkter);
+    const raster = decodeURI(req.query.raster);
     if (!punkter || !raster) return usage(req, res, examples.grid1d);
     const punkterPath = path.join(config.dataPath, punkter);
     const rasterPath = path.join(config.dataPath, raster);
@@ -15,7 +15,7 @@ module.exports = function(app, config) {
   });
   app.get("*", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    const url = req.path;
+    const url = decodeURI(req.path);
     const rasterPath = path.join(config.dataPath, url);
     api.stat1d(rasterPath).then(stat => {
       res.send(stat);
